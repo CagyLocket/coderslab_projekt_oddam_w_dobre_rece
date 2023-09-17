9e5f289db90d23d41ebfd241f8d73b6aa828e780
pawelwrzesien <rsc.pawel@gmail.com>
2023-09-17 19:14:31 +0200

    #2 Strona główna - Dynamiczne statystyki zrobione

--- charity_project/charity/views.py
+++ charity_project/charity/views.py
@@ -1,16 +1,25 @@
+from django.db.models import Count, Sum
 from django.shortcuts import render, redirect
 from django.contrib.auth import get_user_model
 from django.urls import reverse_lazy, reverse
 from django.views import View
 from django.views.generic import ListView, FormView, RedirectView, CreateView, DetailView, UpdateView, DeleteView
 from django.core.paginator import Paginator
+from .models import Donation, Institution
 
 User = get_user_model()
 
 
 class LandingPage(View):
     def get(self, request, *args, **kwargs):
-        return render(request, "charity/index.html", {})
+        donated_bags = Donation.objects.aggregate(total_bags=Sum('quantity'))['total_bags']
+        supported_organizations = Institution.objects.count()
+
+        context = {
+            'donated_bags': donated_bags,
+            'supported_organizations': supported_organizations,
+        }
+        return render(request, "charity/index.html", context)
 
 
 class Login(View):
